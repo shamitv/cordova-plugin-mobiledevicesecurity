1c705ad43b4e4f8e9f1b1f7cfdc7179207c95ae2
Shamit Verma <github@shamit.in>
2021-11-07 17:50:37 +0530

    Fix typo

--- www/MobileDeviceSecurity.js
+++ www/MobileDeviceSecurity.js
@@ -8,8 +8,8 @@ exports.IsJailBreakDevice = function (success, error) {
     exec(success, error, 'MobileDeviceSecurity', 'IsJailBreakDevice');
 };
 
-exports.IsDebugerConnected = function (success, error) {
-    exec(success, error, 'MobileDeviceSecurity', 'IsDebugerConnected');
+exports.IsDebuggerConnected = function (success, error) {
+    exec(success, error, 'MobileDeviceSecurity', 'IsDebuggerConnected');
 };
 
 
