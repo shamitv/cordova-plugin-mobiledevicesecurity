88624a0dea30e774cc6378f8ec08c67124574750
Shamit Verma <github@shamit.in>
2021-11-07 16:23:19 +0530

    Integration of Debug check and jailbrake check

---
+++ src/ios/MobileDeviceSecurity.swift
@@ -0,0 +1,42 @@
+import Cordova
+import Cordova
+import Foundation
+
+@objc(MobileDeviceSecurity)
+class MobileDeviceSecurity : CDVPlugin{
+
+    @objc(IsDebuggerConnected:)
+    func IsDebuggerConnected(command : CDVInvokedUrlCommand){
+        let debugStatus = amIDebugged()
+        let result = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: debugStatus)
+        self.commandDelegate.send(result, callbackId: command.callbackId)
+    }
+
+    @objc(IsJailBreakDevice:)
+    func IsJailBreakDevice(command : CDVInvokedUrlCommand){
+        let check = IOSSecuritySuite.amIJailbrokenWithFailMessage();
+        let result = check.jailbroken
+        let message = check.failMessage
+        var cordovaResult:CDVPluginResult
+        cordovaResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: result)
+        self.commandDelegate.send(cordovaResult, callbackId: command.callbackId)
+    }
+
+    
+    func amIDebugged() -> Bool {
+
+        var kinfo = kinfo_proc()
+        var mib: [Int32] = [CTL_KERN, KERN_PROC, KERN_PROC_PID, getpid()]
+        var size = MemoryLayout<kinfo_proc>.stride
+        let sysctlRet = sysctl(&mib, UInt32(mib.count), &kinfo, &size, nil, 0)
+
+        if sysctlRet != 0 {
+            print("Error occured when calling sysctl(). The debugger check may not be reliable")
+        }
+
+        return (kinfo.kp_proc.p_flag & P_TRACED) != 0
+    }
+    
+    
+
+}
--- www/MobileDeviceSecurity.js
+++ www/MobileDeviceSecurity.js
@@ -1,5 +1,15 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'MobileDeviceSecurity', 'coolMethod', [arg0]);
+exports.IsDebugBuild = function (success, error) {
+    exec(success, error, 'IsDebug', 'IsDebugBuild');
 };
+
+exports.IsJailBreakDevice = function (success, error) {
+    exec(success, error, 'MobileDeviceSecurity', 'IsJailBreakDevice');
+};
+
+exports.IsDebugerConnected = function (success, error) {
+    exec(success, error, 'MobileDeviceSecurity', 'IsDebugerConnected');
+};
+
+
